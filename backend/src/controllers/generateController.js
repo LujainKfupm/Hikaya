0f0202ea9887ed7e57e3edc0a5db226064c14d98
Your GitHub Name <s202249140@kfupm.edu.sa>
2025-11-29 15:16:15 +0300

    connected to OPENAI api

--- backend/src/controllers/generateController.js
+++ backend/src/controllers/generateController.js
@@ -23,12 +23,17 @@
  * }
  */
 
+import dotenv from "dotenv";
 import OpenAI from "openai";
 import Story from "../models/Story.js";
+
+dotenv.config();
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
+
+
 
 function buildStoryPrompt({ heroName, age, gender, topics, morals, details }) {
     const genderText =
