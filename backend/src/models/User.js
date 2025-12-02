69e08bae5ba398f0d8b74fc85edc937d2f430ff4
Fajer <s202280160@kfupm.edu.sa>
2025-12-02 22:27:21 +0300

    enabling password verification in User schema

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -57,6 +57,9 @@ userSchema.pre("save", async function () {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 });
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return bcrypt.compare(enteredPassword, this.password);
+};
 
 
 export default mongoose.model("User", userSchema);
