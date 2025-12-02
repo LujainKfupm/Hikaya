365e3d6fc040c9b9ae1da15f20758c76717d4d65
Your GitHub Name <s202249140@kfupm.edu.sa>
2025-12-02 23:19:20 +0300

    updated auth

--- backend/src/routes/generateRoutes.js
+++ backend/src/routes/generateRoutes.js
@@ -1,11 +1,9 @@
 
 import express from "express";
 import { generateStory } from "../controllers/generateController.js";
-import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/story", protect, generateStory);
 router.post("/story", generateStory);
 
 export default router;
