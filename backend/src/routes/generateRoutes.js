82be8fd7e0bc40922d9c17a76a470aef12998721
Your GitHub Name <s202249140@kfupm.edu.sa>
2025-12-04 19:04:46 +0300

    fixed guest story generation bug

--- backend/src/routes/generateRoutes.js
+++ backend/src/routes/generateRoutes.js
@@ -1,11 +1,11 @@
 
 import express from "express";
 import { generateStory } from "../controllers/generateController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { optionalAuth } from "../middleware/authMiddleware.js";
 
 
 const router = express.Router();
 
-router.post("/story", protect, generateStory);
+router.post("/story", optionalAuth, generateStory);
 
 export default router;
