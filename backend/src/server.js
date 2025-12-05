d5347fabb13dc69b5056b5f045f604d63b4057e8
amira <abidoamira@gmail.com>
2025-12-05 20:04:16 +0300

    resolve conflicts

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,25 +23,12 @@ import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import storyRoutes from "./routes/storyRoutes.js";
 import generateRoutes from "./routes/generateRoutes.js";
-import {AdminAccount} from "./utils/adminAccount.js";
+import { AdminAccount } from "./utils/adminAccount.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-// Connect DB
-
-async function startServer() {
-    await connectDB(process.env.MONGO_URL);
-    await AdminAccount();
-
-    app.listen(process.env.PORT || 3000, () => {
-        console.log("Server running");
-    });
-}
-
-startServer();
 
 // Route Mounting
 app.use("/api/auth", authRoutes);
@@ -52,3 +39,14 @@ app.get("/api/health", (req, res) => {
     res.json({ status: "ok" });
 });
 
+// Connect DB & start server
+async function startServer() {
+    await connectDB(process.env.MONGO_URL);
+    await AdminAccount();
+
+    app.listen(process.env.PORT || 3000, () => {
+        console.log("Server running");
+    });
+}
+
+startServer();
